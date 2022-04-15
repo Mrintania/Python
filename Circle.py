7b0d0136c7ea474b31c0cd79f6bf2c0562f8cff7
mrintania <95394889+Mrintania@users.noreply.github.com>
2022-04-15 20:05:16 +0700

    first commit

---
+++ Circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+r = float(input("In put radian : "))
+print("Circle area is : ", math.pi*r*r)
+print(math.pi)
+print(math.pi)
+print(math.pi)
+print(math.pi)
+print(math.pi)
+print(math.pi)
+print(math.pi)
